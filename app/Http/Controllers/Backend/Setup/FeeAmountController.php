b69a43c2fd0465bff132b5c9ef820144d007f8ff
Mayl Meto <maylme@protonmail.com>
2021-06-16 13:40:31 +0545

    fee amount category done

---
+++ app/Http/Controllers/Backend/Setup/FeeAmountController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\FeeCategoryAmount;
+use App\Models\StudentClass;
+use App\Models\FeeCategory;
+
+class FeeAmountController extends Controller
+{
+    public function ViewFeeAmount(){
+
+      $data['allData'] = FeeCategoryAmount::all();
+        return view('backend.setup.fee_amount.view_fee_amount', $data);
+      }
+
+      public function AddFeeAmount(){
+        $data['fee_categories'] = FeeCategory::all();
+        $data['classes'] = StudentClass::all();
+        return view('backend.setup.fee_amount.fee_amount_add', $data);
+      }
+
+    }
+
