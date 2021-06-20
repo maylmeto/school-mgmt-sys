abb4115aa6473966f37b08ed4d664161d9d01965
Mayl Meto <maylme@protonmail.com>
2021-06-20 14:18:45 +0545

    student registration ui added

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -4,8 +4,43 @@ namespace App\Http\Controllers\Backend\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AssignStudent;
+use App\Models\User;
+use App\Models\DiscountStudent;
+use App\Models\StudentClass;
+use App\Models\StudentGroup;
+use App\Models\StudentShift;
+use App\Models\StudentYear;
 
 class StudentRegController extends Controller
 {
-    //
+  public function StudentRegView(){
+
+    $data['allData'] = AssignStudent::all();
+    return view('backend.student.student_reg.student_view', $data);
+
+  }
+
+  public function StudentRegAdd(){
+    $data['years'] = StudentYear::all();
+    $data['classes'] = StudentClass::all();
+    $data['groups'] = StudentGroup::all();
+    $data['shifts'] = StudentShift::all();
+
+    return view('backend.student.student_reg.student_add',$data);
+
+  }
+
+
+  public function StudentRegStore(Request $request){
+
+
+
+
+
+
+
+
+
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -150,11 +150,12 @@ Route::get('designation/delete/{id}', [DesignationController::class, 'Designatio
 
   });
 
-  // Student Management routes
+  // Student Registration routes
 Route::prefix('students')->group(function(){
 
 Route::get('/reg/view', [StudentRegController::class, 'StudentRegView'])->name('student.registration.view');
-
+Route::get('/reg/add', [StudentRegController::class, 'StudentRegAdd'])->name('student.registration.add');
+Route::post('/reg/store', [StudentRegController::class, 'StudentRegStore'])->name('store.student.registration');
 
   });
 
